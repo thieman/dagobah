2a59ccd869d67ecbb4fa6efea8824c56baad2950
tthieman <travis.thieman@gmail.com>
2013-05-14 17:20:01 -0400

    Make some progress on the job detail

--- dagobah/daemon/views.py
+++ dagobah/daemon/views.py
@@ -25,3 +25,12 @@ def jobs():
     """ Show information on all known Jobs. """
     return render_template('jobs.html',
                            jobs=get_jobs())
+
+
+@app.route('/job/<job_id>', methods=['GET'])
+def job_detail(job_id=None):
+    """ Show a detailed description of a Job's status. """
+    jobs = get_jobs()
+    print jobs
+    job = [job for job in jobs if str(job['job_id']) == job_id][0]
+    return render_template('job_detail.html', job=job)
