e93609909acc5f59b97806629c8526cef4140e74
Ryan Clough <ryanc@vimeo.com>
2014-05-14 10:31:22 -0400

    alembic migration

---
+++ dagobah/backend/migrations/versions/4de8f69a75d1_adding_notes_column.py
@@ -0,0 +1,22 @@
+"""adding notes column
+
+Revision ID: 4de8f69a75d1
+Revises: 2ab7af991b87
+Create Date: 2013-10-29 15:43:13.132113
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '4de8f69a75d1'
+down_revision = '2ab7af991b87'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('dagobah_job', sa.Column('notes', sa.String(1000)))
+
+
+def downgrade():
+    op.drop_column('dagobah_job', 'notes')
