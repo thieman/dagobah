606de6d94f0e916cf599a6774d37285ece6259f8
Travis Thieman <travis.thieman@gmail.com>
2014-06-07 12:56:15 -0400

    Fix stdout leak in echo_dagobah_conf command. Closes #73.

--- dagobah/daemon/daemon.py
+++ dagobah/daemon/daemon.py
@@ -8,6 +8,7 @@ from flask import Flask, send_from_directory
 from flask_login import LoginManager
 import yaml
 
+from .. import return_standard_conf
 from ..core import Dagobah, EventHandler
 from ..email import get_email_handler
 
@@ -74,21 +75,6 @@ def get_config_file():
     new_config.close()
     replace_nones(config)
     return config
-
-
-def print_standard_conf():
-    """ Print the sample config file to stdout. """
-    print return_standard_conf()
-
-
-
-def return_standard_conf():
-    """ Return the sample config file. """
-    config_file = open(os.path.join(location, 'dagobahd.yml'))
-    result = config_file.read()
-    config_file.close()
-    result = result % {'app_secret': os.urandom(24).encode('hex')}
-    return result
 
 
 def configure_app():
