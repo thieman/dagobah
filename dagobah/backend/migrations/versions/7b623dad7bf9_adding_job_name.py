4edd1e07c3fc53193b5ff35741621e48e93a7859
Ryan Clough <ryanc@vimeo.com>
2014-06-17 13:29:26 -0400

    Alembic migration to add a job name

---
+++ dagobah/backend/migrations/versions/7b623dad7bf9_adding_job_name.py
@@ -0,0 +1,25 @@
+"""adding notes column
+
+Revision ID: 7b623dad7bf9
+Revises: d455481ec6e4
+Create Date: 2014-06-16 15:35:00.00023
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '7b623dad7bf9'
+down_revision = 'd455481ec6e4'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    conn = op.get_bind()
+    columns = conn.execute('select * from dagobah_task limit 1')._metadata.keys
+    if 'hostname' not in columns:
+        op.add_column('dagobah_task', sa.Column('job_name', sa.String(1000)))
+
+
+def downgrade():
+    op.drop_column('dagobah_task', 'job_name')
