4e9e98fdac066eff105bd1b11c45c41fd9ec7a9b
Ryan Clough <ryanc@vimeo.com>
2015-06-04 12:47:36 -0400

    Fixing variable names and correctly referencing them

--- dagobah/core/core.py
+++ dagobah/core/core.py
@@ -1168,14 +1168,14 @@ class Task(object):
 
 class JobTask(object):
     """ Expandable Task that references a job """
-    def __init__(self, parent_job, job_name, task_name):
+    def __init__(self, parent_job, target_job_name, task_name):
         self.parent_job = parent_job
-        self.job_name = job_name
+        self.target_job_name = target_job_name
         self.name = task_name
 
     def expand(self):
         """ Expand this JobTask into a list of tasks """
-        return self.parent.parent.get_job(self.job_name).tasks.values()
+        return self.parent_job.parent.get_job(self.target_job_name).tasks.values()
 
     def _serialize(self, include_run_logs=False, strict_json=False):
         """ Serialize a representation of this Task to a Python dict. """
