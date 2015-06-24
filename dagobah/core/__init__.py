12e4139ec46b800a8e0effbd049d881c31fa9ff8
Ryan Clough <ryanc@vimeo.com>
2015-06-24 18:31:38 -0400

    Missed a couple of DagobahError import corrections

--- dagobah/core/__init__.py
+++ dagobah/core/__init__.py
@@ -1,6 +1,6 @@
 from dag import DAG, DAGValidationError
 from .components import JobState, Scheduler, EventHandler
-from .dagobah import DagobahError
+from .dagobah_error import DagobahError
 from .dagobah import Dagobah
 from .task import Task
 from .jobtask import JobTask
