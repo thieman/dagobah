e9a6a1a94015949789f76ad3af9a2e4a107f0289
Ryan Clough <ryanc@vimeo.com>
2015-10-09 10:39:24 -0400

    Fixing comment

--- dagobah/core/dagobah.py
+++ dagobah/core/dagobah.py
@@ -100,7 +100,7 @@ class Dagobah(object):
             job.schedule(job_json['cron_schedule'])
 
         for task in job_json.get('tasks', []):
-            # If it is a jobtask, it will have a job_id
+            # If it is a jobtask, it will have a job_name
             if task.get('job_name'):
                 self.add_jobtask_to_job(job, task['job_name'], str(task['name']))
             else:
