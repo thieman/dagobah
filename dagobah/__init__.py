13e2866f4b9ac08a46a84b65a1db3d912c4078db
Srikrishna Sivesh Guttula <kittusrikrishnasivesh@gmail.com>
2019-05-14 22:36:13 +0530

    avoid bytes string formatting in python 3

--- dagobah/__init__.py
+++ dagobah/__init__.py
@@ -12,6 +12,6 @@ def print_standard_conf():
 
 def return_standard_conf():
     """ Return the sample config file. """
-    result = resource_string(__name__, 'daemon/dagobahd.yml')
+    result = resource_string(__name__, 'daemon/dagobahd.yml').decode('ascii')
     result = result % {'app_secret': os.urandom(24).encode('hex')}
     return result
